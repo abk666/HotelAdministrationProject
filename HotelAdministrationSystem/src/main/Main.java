6a7c37a131f7e1eb31050e35cc2ac3c2ba131dd1
KMMS <KMMS@DESKTOP-DBD1GAU>
2021-07-21 00:34:36 +0630

    login form

--- HotelAdministrationSystem/src/main/Main.java
+++ HotelAdministrationSystem/src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import database.DbConnection;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
@@ -12,8 +13,10 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			
+	    	primaryStage.setResizable(false);
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("LogInUI.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
