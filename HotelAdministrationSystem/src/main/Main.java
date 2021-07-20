35c36c1192b9616944a5793a664ec9b1a1fa0c7b
KMMS <KMMS@DESKTOP-DBD1GAU>
2021-07-21 00:51:08 +0630

    login form day one

--- HotelAdministrationSystem/src/main/Main.java
+++ HotelAdministrationSystem/src/main/Main.java
@@ -13,12 +13,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			
-	    	primaryStage.setResizable(false);
+//			primaryStage = (Stage)primaryStage.getScene().getWindow();
+//			primaryStage.setResizable(false);
+	    	
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("LogInUI.fxml"));
-			Scene scene = new Scene(root);
+			Scene scene = new Scene(root,800,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
