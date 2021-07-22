c0f941e95ce49e291497ad8eef4ec3f51b9f9f9d
abk666 <62738475+abk666@users.noreply.github.com>
2021-07-22 23:55:37 +0630

    LogIn form error fixing

--- HotelAdministrationSystem/src/main/LogInController.java
+++ HotelAdministrationSystem/src/main/LogInController.java
@@ -43,95 +43,96 @@ public class LogInController implements Initializable {
     private final CheckUserCredentials checkUser = new CheckUserCredentials();
     @FXML
     void processLogin(ActionEvent event) throws SQLException, IOException {
-    	
-    	String email = tfEmail.getText().trim();
-    	String password = pfPassword.getText();
-    	String role = cobStaff.getValue();
-    	String loginType = "Email";
-    	
-    	if(checkUser.isUserValid(email,password,role,loginType)) {
-    		
-    		switch (role) {
-			case "Admin":
-				Stage adminStage = (Stage) ((Button)event.getSource()).getScene().getWindow();
-				adminStage.hide();
-				
-				AnchorPane adminRoot = (AnchorPane)FXMLLoader.load(getClass().getResource("../admin/AdminMainUI.fxml"));
-				Scene adminScene = new Scene(adminRoot);
-				adminStage.setTitle("Admin Main Section");
-				adminStage.setScene(adminScene);
-				adminStage.show();
-				
-				break;
-				
-				
-			case "Manager":
-				Stage managerStage = (Stage) ((Button)event.getSource()).getScene().getWindow();
-				managerStage.hide();
-				
-				AnchorPane managerRoot = (AnchorPane)FXMLLoader.load(getClass().getResource("../manager/ManagerMainUI.fxml"));
-				Scene managerScene = new Scene(managerRoot);
-				managerStage.setTitle("Manager Main Section");
-				managerStage.setScene(managerScene);
-				managerStage.show();
-			
-				break;
-				
-			case "Reception":
-				Stage receptionStage = (Stage) ((Button)event.getSource()).getScene().getWindow();
-				receptionStage.hide();
-				
-				AnchorPane receptionRoot = (AnchorPane)FXMLLoader.load(getClass().getResource("../reception/ReceptionMainUI.fxml"));
-				Scene receptionScene = new Scene(receptionRoot);
-				receptionStage.setTitle("Reception Main Section");
-				receptionStage.setScene(receptionScene);
-				receptionStage.show();
-			
-				break;
-				
-			case "HouseKeeping":
-				Stage housekeepingStage = (Stage) ((Button)event.getSource()).getScene().getWindow();
-				housekeepingStage.hide();
-				
-				AnchorPane housekeepingRoot = (AnchorPane)FXMLLoader.load(getClass().getResource("../housekeeping/HouseKeepingMainUI.fxml"));
-				Scene housekeepingScene = new Scene(housekeepingRoot);
-				housekeepingStage.setTitle("HouseKeeping Main Section");
-				housekeepingStage.setScene(housekeepingScene);
-				housekeepingStage.show();
-			
-				break;	
-				
-			case "DiningRoom":
-				Stage diningroomStage = (Stage) ((Button)event.getSource()).getScene().getWindow();
-				diningroomStage.hide();
-				
-				AnchorPane diningroomRoot = (AnchorPane)FXMLLoader.load(getClass().getResource("../diningroom/DiningRoomMainUI.fxml"));
-				Scene diningroomScene = new Scene(diningroomRoot);
-				diningroomStage.setTitle("DiningRoom Main Section");
-				diningroomStage.setScene(diningroomScene);
-				diningroomStage.show();
-			
-				break;		
-				
-			case "Accountant":
-				Stage accountantStage = (Stage) ((Button)event.getSource()).getScene().getWindow();
-				accountantStage.hide();
-				
-				AnchorPane accountantRoot = (AnchorPane)FXMLLoader.load(getClass().getResource("../accountant/AccountantMainUI.fxml"));
-				Scene accountantScene = new Scene(accountantRoot);
-				accountantStage.setTitle("Accountant Main Section");
-				accountantStage.setScene(accountantScene);
-				accountantStage.show();
-			
-				break;			
-			}
-    		
+    	if(tfEmail.getText().contains("@")&& !tfEmail.getText().trim().isEmpty() &&!pfPassword.getText().isEmpty() && cobStaff.getValue()!=null) {
+    		String email = tfEmail.getText().trim();
+        	String password = pfPassword.getText();
+        	String role = cobStaff.getValue();
+        	String loginType = "Email";
+        	
+        	if(checkUser.isUserValid(email,password,role,loginType)) {
+        		
+        		switch (role) {
+    			case "Admin":
+    				Stage adminStage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+    				adminStage.hide();
+    				
+    				AnchorPane adminRoot = (AnchorPane)FXMLLoader.load(getClass().getResource("../admin/AdminMainUI.fxml"));
+    				Scene adminScene = new Scene(adminRoot);
+    				adminStage.setTitle("Admin Main Section");
+    				adminStage.setScene(adminScene);
+    				adminStage.show();
+    				
+    				break;
+    				
+    				
+    			case "Manager":
+    				Stage managerStage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+    				managerStage.hide();
+    				
+    				AnchorPane managerRoot = (AnchorPane)FXMLLoader.load(getClass().getResource("../manager/ManagerMainUI.fxml"));
+    				Scene managerScene = new Scene(managerRoot);
+    				managerStage.setTitle("Manager Main Section");
+    				managerStage.setScene(managerScene);
+    				managerStage.show();
+    			
+    				break;
+    				
+    			case "Reception":
+    				Stage receptionStage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+    				receptionStage.hide();
+    				
+    				AnchorPane receptionRoot = (AnchorPane)FXMLLoader.load(getClass().getResource("../reception/ReceptionMainUI.fxml"));
+    				Scene receptionScene = new Scene(receptionRoot);
+    				receptionStage.setTitle("Reception Main Section");
+    				receptionStage.setScene(receptionScene);
+    				receptionStage.show();
+    			
+    				break;
+    				
+    			case "HouseKeeping":
+    				Stage housekeepingStage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+    				housekeepingStage.hide();
+    				
+    				AnchorPane housekeepingRoot = (AnchorPane)FXMLLoader.load(getClass().getResource("../housekeeping/HouseKeepingMainUI.fxml"));
+    				Scene housekeepingScene = new Scene(housekeepingRoot);
+    				housekeepingStage.setTitle("HouseKeeping Main Section");
+    				housekeepingStage.setScene(housekeepingScene);
+    				housekeepingStage.show();
+    			
+    				break;	
+    				
+    			case "DiningRoom":
+    				Stage diningroomStage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+    				diningroomStage.hide();
+    				
+    				AnchorPane diningroomRoot = (AnchorPane)FXMLLoader.load(getClass().getResource("../diningroom/DiningRoomMainUI.fxml"));
+    				Scene diningroomScene = new Scene(diningroomRoot);
+    				diningroomStage.setTitle("DiningRoom Main Section");
+    				diningroomStage.setScene(diningroomScene);
+    				diningroomStage.show();
+    			
+    				break;		
+    				
+    			case "Accountant":
+    				Stage accountantStage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+    				accountantStage.hide();
+    				
+    				AnchorPane accountantRoot = (AnchorPane)FXMLLoader.load(getClass().getResource("../accountant/AccountantMainUI.fxml"));
+    				Scene accountantScene = new Scene(accountantRoot);
+    				accountantStage.setTitle("Accountant Main Section");
+    				accountantStage.setScene(accountantScene);
+    				accountantStage.show();
+    			
+    				break;			
+    			}
+        		
+        	}
     	}
     	
-    	else {
     	
-//    		lblStatus.setText("Login Fail");
-//    		lblStatus.setTextFill(Paint.valueOf("Red"));
+    	else {
+    	System.out.println("Input fields must not be null");
+
     	}
 
     }
